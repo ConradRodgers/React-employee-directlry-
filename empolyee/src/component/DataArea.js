cd1fa5599ee5fdf92ba929e2fcd867f1f05dfe5e
Conrad Rodgers <conrad13500@gmail.com>
2020-05-27 14:30:07 -0700

    update

--- empolyee/src/component/DataArea.js
+++ empolyee/src/component/DataArea.js
@@ -1,7 +1,6 @@
 import React from "react";
-
 import API from "../api.js";
-
+import Table from "react-bootstrap/Table";
 class DataArea extends React.Component {
   state = { users: [] };
   componentDidMount() {
@@ -10,8 +9,25 @@ class DataArea extends React.Component {
     });
   }
   render() {
-    return <h1>{this.state.users.map((x) => x.email)}</h1>;
+    return(
+    {this.state.users.map((x) => {
+        return (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>dob</th>
+                <th>Name</th>
+                <th>Cell</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </Table>
+          //return <h1>{this.state.users.map((x) => x.email)}</h1>;
+        );
+      });
+    }
   }
 }
-
+    )
 export default DataArea;
